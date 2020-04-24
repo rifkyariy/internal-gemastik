9eb2f671f1fed933e25d3e4f8e8879e37eb29c2e
rifkyariy <rifkyariy@gmail.com>
2020-04-25 05:10:43 +0700

    fix bottom navbar problem on mobile screen again

--- public/js/contentdata.js
+++ public/js/contentdata.js
@@ -385,7 +385,9 @@ initFaq(4);
 
 // after all sets
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+// responsive mobile view
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
